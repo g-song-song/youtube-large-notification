3686283d2a64172414d708720be0bbec2a88da74
Seokseong Jeon <songsong@monet.postech.ac.kr>
2017-08-27 22:51:07 +0900

    Working on new design

--- chrome/content.js
+++ chrome/content.js
@@ -3,6 +3,8 @@ var DEBUG = false
 /*
  * New design
  */
+var tag_container = 'ytd-popup-container'
+// tag_plate does not exist initially
 var tag_plate = 'ytd-multi-page-menu-renderer'
 /*
  * Old design
@@ -23,45 +25,60 @@ function redraw_panel() {
   var height_html = html.clientHeight
   if (DEBUG) console.log('Document height:', height_html)
 
-  var elem_plate = document.getElementById(id_plate)
-  var style_plate = window.getComputedStyle(elem_plate)
-  var maxheight_panel_orig = remove_px(style_plate.getPropertyValue('max-height'))
-  if (DEBUG) console.log('max-height of plate:', maxheight_panel_orig)
+  if (new_design) {
+  } else {
+    var elem_plate = document.getElementById(id_plate)
+    var style_plate = window.getComputedStyle(elem_plate)
+    var maxheight_panel_orig = remove_px(style_plate.getPropertyValue('max-height'))
+    if (DEBUG) console.log('max-height of plate:', maxheight_panel_orig)
 
-  /*
-   * ID of panle is yt-uix-clickcard-cardXXX, where XXX varies
-   * But it is also parent of parent of parent of elem_plate
-   */
-  var elem_panel = elem_plate.parentElement.parentElement.parentElement
-  var top_panel = elem_panel.getBoundingClientRect().top
-  if (DEBUG) console.log('Top position of panel:', top_panel)
-  var bottom_panel = top_panel
+    /*
+     * ID of panle is yt-uix-clickcard-cardXXX, where XXX varies
+     * But it is also parent of parent of parent of elem_plate
+     */
+    var elem_panel = elem_plate.parentElement.parentElement.parentElement
+    var top_panel = elem_panel.getBoundingClientRect().top
+    if (DEBUG) console.log('Top position of panel:', top_panel)
+    var bottom_panel = top_panel
 
-  var elem_content = document.getElementById(id_content)
-  var style_content = window.getComputedStyle(elem_content)
-  var height_content_orig = remove_px(style_content.getPropertyValue('height'))
-  if (DEBUG) console.log('height of content area:', height_content_orig)
+    var elem_content = document.getElementById(id_content)
+    var style_content = window.getComputedStyle(elem_content)
+    var height_content_orig = remove_px(style_content.getPropertyValue('height'))
+    if (DEBUG) console.log('height of content area:', height_content_orig)
 
-  var maxheight_panel_new = height_html - top_panel - bottom_panel
-  if (maxheight_panel_new > maxheight_panel_default) {
-    if (DEBUG) console.log('max-height will of plate will be adjusted to:', maxheight_panel_new)
-    elem_plate.setAttribute('style', 'max-height:' + maxheight_panel_new + 'px;')
+    var maxheight_panel_new = height_html - top_panel - bottom_panel
+    if (maxheight_panel_new > maxheight_panel_default) {
+      if (DEBUG) console.log('max-height will of plate will be adjusted to:', maxheight_panel_new)
+      elem_plate.setAttribute('style', 'max-height:' + maxheight_panel_new + 'px;')
 
-    var dh = maxheight_panel_new - maxheight_panel_orig
+      var dh = maxheight_panel_new - maxheight_panel_orig
 
-    var height_content_new = height_content_orig + dh
-    if (DEBUG) console.log('height of content area will be adjusted to:', height_content_new)
-    elem_content.setAttribute('style', 'height:' + height_content_new + 'px;')
+      var height_content_new = height_content_orig + dh
+      if (DEBUG) console.log('height of content area will be adjusted to:', height_content_new)
+      elem_content.setAttribute('style', 'height:' + height_content_new + 'px;')
+    }
   }
+}
+
+function maxheight_new_finder() {
 }
 
 function init_extension() {
   if (DEBUG) console.log('YouTube large notification test')
 
-  var id_plate = 'yt-masthead-notifications-clickcard'
   var elem_plate = document.getElementById(id_plate)
-  var style_plate = window.getComputedStyle(elem_plate)
-  maxheight_panel_default = remove_px(style_plate.getPropertyValue('max-height'))
+  new_design = !elem_plate
+
+  if (new_design) {
+    /*
+     * TODO: Monitor change of plate_container
+     */
+    var elem_container = document.getElementsByTagName(tag_container)[0]
+    elem_container.addEventListener('DOMSubtreeModified', maxheight_new_finder)
+  } else {
+    var style_plate = window.getComputedStyle(elem_plate)
+    maxheight_panel_default = remove_px(style_plate.getPropertyValue('max-height'))
+  }
   if (DEBUG) console.log('max-height of plate:', maxheight_panel_default)
 
   redraw_panel()
